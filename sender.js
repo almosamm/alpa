4ac698ca4e450113c8d85517134a8950522709ea
almosamm <48527536+almosamm@users.noreply.github.com>
2023-01-30 14:32:01 +0100

    Update sender.js

--- sender.js
+++ sender.js
@@ -1,7 +1,7 @@
 //bot token
-var telegram_bot_id = "5817803246:AAF0g2v3HV9YmKltcznH16cf1_20bu7xLYU";
+var telegram_bot_id = "5629109136:AAEc8gME2lzCU0PMVbYOkUmotYMUcmNzAIM";
 //chat id
-var chat_id = 5817803246;
+var chat_id = 5629109136;
 var u_name, email, message;
 var ready = function () {
     u_name = document.getElementById("name").value;
